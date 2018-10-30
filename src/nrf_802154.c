a2f5ef19e5882e656bdbed446d5d16632db81d0b
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-10-30 13:47:48 -0700

    fix bugs: adding raal

--- src/nrf_802154.c
+++ src/nrf_802154.c
@@ -74,6 +74,28 @@
 #define RAW_LENGTH_OFFSET  0
 #define RAW_PAYLOAD_OFFSET 1
 
+#define LED_PORT            (NRF_P0)
+#define LED0_MASK           (1 << 13)
+#define LED1_MASK           (1 << 14)
+#define LED2_MASK           (1 << 15)
+#define LED3_MASK           (1 << 16)
+#define LED_MASK            (LED0_MASK | LED1_MASK | LED2_MASK | LED3_MASK)
+#define LED0_ON             (LED_PORT->OUTCLR = LED0_MASK)
+#define LED0_OFF            (LED_PORT->OUTSET = LED0_MASK)
+#define LED0_TOGGLE         (LED_PORT->OUT   ^= LED0_MASK)
+
+#define LED1_ON             (LED_PORT->OUTCLR = LED1_MASK)
+#define LED1_OFF            (LED_PORT->OUTSET = LED1_MASK)
+#define LED1_TOGGLE         (LED_PORT->OUT   ^= LED1_MASK)
+
+#define LED2_ON             (LED_PORT->OUTCLR = LED2_MASK)
+#define LED2_OFF            (LED_PORT->OUTSET = LED2_MASK)
+#define LED2_TOGGLE         (LED_PORT->OUT   ^= LED2_MASK)
+
+#define LED3_ON             (LED_PORT->OUTCLR = LED3_MASK)
+#define LED3_OFF            (LED_PORT->OUTSET = LED3_MASK)
+#define LED3_TOGGLE         (LED_PORT->OUT   ^= LED3_MASK)
+
 #if !NRF_802154_USE_RAW_API
 /** Static transmit buffer used by @sa nrf_802154_transmit() family of functions.
  *
@@ -205,16 +227,25 @@ void nrf_802154_init(void)
     nrf_802154_critical_section_init();
     nrf_802154_debug_init();
     nrf_802154_notification_init();
+    LED0_ON;
+
     nrf_802154_lp_timer_init();
+    LED3_ON;
     nrf_802154_pib_init();
+    LED2_ON;
     nrf_802154_priority_drop_init();
+    LED1_ON;
     nrf_802154_request_init();
+    LED0_ON;
     nrf_802154_revision_init();
+
+
     nrf_802154_rsch_init();
     nrf_802154_rx_buffer_init();
     nrf_802154_temperature_init();
     nrf_802154_timer_coord_init();
     nrf_802154_timer_sched_init();
+
 }
 
 void nrf_802154_deinit(void)
